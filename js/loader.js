e29cabe1be3f42407b8085d9ceca3d847f7f6f36
cbaptiste <baptiste.chevrin@ynov.com>
2026-03-23 15:21:34 +0100

    parserCSV + data

--- js/loader.js
+++ js/loader.js
@@ -1,21 +1,105 @@
 const boutonurl = document.getElementById('btnRechercheURL');
-const boutoncsv = document.getElementById('btnRechercheCSV');
+const boutonTexte = document.getElementById('btnAnalyserTexte');
 test = document.getElementById('test');
 test2 = document.getElementById('test2')
+testfile = document.getElementById('testfile')
 
-boutonurl.addEventListener('click', () => {
-  const valeur = document.getElementById('URL_finder').value;
-  test.textContent = valeur
-  console.log(valeur);
+async function loadFromURL (url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error ( 'HTTP $ {response. status}');
+        const contentType = response.headers.get ('Content-Type') ;
+        if (contentType.includes ('json') ) {
+            return await response.json();
+        } else {
+            const text = await response.text();
+            return parseCSV(text); // Voir ci-dessous
+        }
+    } catch (error) {
+        console.error("Erreur de chargement :", error);
+    }
+}
+
+function loadFromFile (file) {
+    return new Promise ( (resolve, reject) => {
+        const reader = new FileReader () ;
+        reader.onload = (e) => resolve(e.target.result) ;
+        reader.onerror = () => reject (new Error('Lecture impossible'));
+        reader.readAsText (file, 'UTF-8');
+    });
+}
+
+function loadFromText (rawText) { /* parser directement */ }
+
+
+function parserCSV (csv) {
+  const res = []
+  const lignes = csv.split('\n')
+  const names = lignes[0].split(',')
+  for (let i = 1; i < lignes.length ; i++) {
+    const Obj = {}
+    const categories = lignes[i].split(',')
+    for (let y in names) {
+      Obj[names[y]] = categories[y]
+    }
+    res.push(Obj)
+  }
+  return res
+}
+
+function GetStaticData (file) {
+  const res = []
+  const parserFile = parserCSV(file)
+  for (let i = 0; i < parserFile.length; i++) {
+    if (typeof parserFile[i] == "number") {
+
+    }
+  }
+
+}
+
+// Va vérifier si il s'agit d'une URL
+function estUneURL(texte) {
+  try {
+    new URL(texte);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+
+boutonurl.addEventListener('click', async () => { 
+  const valeururl = document.getElementById('URL_finder').value;
+  
+  if (estUneURL(valeururl)) {
+    const resulturl = await loadFromURL(valeururl);
+    test.textContent = JSON.stringify(resulturl);
+  } else {
+    test.textContent = "Il ne s'agit pas d'une URL";
+  }
 });
 
-document.getElementById('monFichier').addEventListener('change', (e) => {
-  const file = e.target.files[0]; 
-  console.log(file);
+document.getElementById('monFichier').addEventListener('change', async (e) => {
+  const file = e.target.files[0];
+
+  if (!file.name.endsWith('.csv') && !file.name.endsWith('.json')) {
+    console.error("Fichier non supporté");
+    return;
+  }
+  const resfile = await loadFromFile(file);
+  if (file.name.endsWith('.csv')){
+    testfile.textContent = JSON.stringify(parserCSV(resfile), null, 2)
+  }
+  else {
+    testfile.textContent = resfile
+  }
 });
 
-boutoncsv.addEventListener('click', () => {
-  const valeur = document.getElementById('CSV_analyser').value;
-  test2.textContent = valeur
-  console.log(valeur);
+
+
+boutonTexte.addEventListener('click', () => {
+  const texteColle = document.getElementById('texte_brut').value;
+  test2.textContent = texteColle;
+  console.log(texteColle);
 });
